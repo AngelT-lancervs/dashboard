4502a1c6141150c17d7add7277b7ccf835dac8ba
Angel Tomalá <108084684+AngelT-lancervs@users.noreply.github.com>
2023-10-31 02:24:18 +0000

    add temperatures and uv

--- public/javascript/load_data.js
+++ public/javascript/load_data.js
@@ -46,3 +46,65 @@ let cargarPrecipitacion = () => {
   }
   
   cargarFechaActual()
+
+  let cargarUv = () => {
+    
+    let actual = fechaActual();
+    let datos = [];
+
+    for(let index= 0; index < tiempoArr.length; index++){
+        const tiempo = tiempoArr[index];
+        const uv = uvArr[index];
+
+        if(tiempo.includes(actual)){
+          datos.push(uv);
+        }
+    }
+
+    let max= Math.max(...datos);
+    let min= Math.min(...datos);
+    let sum= datos.reduce((a,b) => a + b, 0);
+    let prom= (sum/datos.length) || 0;
+
+    let uvMinValue = document.getElementById("uvMinValue")
+    let uvPromValue = document.getElementById("uvPromValue")
+    let uvMaxValue = document.getElementById("uvMaxValue")
+    //Actualice los elementos HTML con los valores correspondientes
+    uvMinValue.textContent = `Min ${min} [mm]`
+    uvPromValue.textContent = `Prom ${ Math.round(prom * 100) / 100 } [mm]`
+    uvMaxValue.textContent = `Max ${max} [mm]`
+
+  }
+
+cargarUv();
+
+let cargarTemperatura = () => {
+    
+  let actual = fechaActual();
+  let datos = [];
+
+  for(let index= 0; index < tiempoArr.length; index++){
+      const tiempo = tiempoArr[index];
+      const tmp = temperaturaArr[index];
+
+      if(tiempo.includes(actual)){
+        datos.push(tmp);
+      }
+  }
+
+  let max= Math.max(...datos);
+  let min= Math.min(...datos);
+  let sum= datos.reduce((a,b) => a + b, 0);
+  let prom= (sum/datos.length) || 0;
+
+  let tmpMinValue = document.getElementById("temperaturaMinValue")
+  let tmpPromValue = document.getElementById("temperaturaPromValue")
+  let tmpMaxValue = document.getElementById("temperaturaMaxValue")
+  //Actualice los elementos HTML con los valores correspondientes
+  tmpMinValue.textContent = `Min ${min} [mm]`
+  tmpPromValue.textContent = `Prom ${ Math.round(prom * 100) / 100 } [mm]`
+  tmpMaxValue.textContent = `Max ${max} [mm]`
+
+}
+
+cargarTemperatura();
